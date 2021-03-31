193c995c7ae1ac0de304f4dabbcf44b4ce543d4e
V LOKESH <49988191+LokeshVeluru@users.noreply.github.com>
2021-03-31 19:03:34 +0530

    Add files via upload

---
+++ ParkingSimulation/ParkingSimulation/Models/FourWheeler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingSimulation.Models
+{
+    class FourWheeler : Vehicle
+    {
+        public FourWheeler(String vehicleNo)
+        {
+            this.Number = vehicleNo;
+            this.Type = VehicleType.FOUR_WHEELER;
+        }
+    }
+}
---
+++ ParkingSimulation/ParkingSimulation/Models/HeavyVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingSimulation.Models
+{
+    class HeavyVehicle : Vehicle
+    {
+        public HeavyVehicle(String vehicleNo)
+        {
+            this.Number = vehicleNo;
+            this.Type = VehicleType.HEAVY_VEHICLE;
+        }
+    }
+}

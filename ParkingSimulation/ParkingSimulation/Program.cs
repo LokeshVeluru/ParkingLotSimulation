6dee187007c5bbabda2ba0e2600580d5c88e5810
V LOKESH <49988191+LokeshVeluru@users.noreply.github.com>
2021-04-05 10:20:25 +0530

    Add files via upload

---
+++ ParkingSimulation/ParkingSimulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using ParkingSimulation.Models;
+using ParkingSimulation.Services;
+
+namespace ParkingSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAppServices service = new AppServices();
+
+            service.CreateParkingLot();
+            service.StartParkingLot();
+        }
+    }
+}
